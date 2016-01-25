7dd2a51b24cdf08accf2b02bcaffe562a36fc735
Sindre Sorhus <sindresorhus@gmail.com>
2016-01-25 22:45:17 +0100

    fix tests

--- test.js
+++ test.js
@@ -2,13 +2,14 @@
 var test = require('ava');
 var childProcess = require('child_process');
 
-test(function (t) {
+test.cb(function (t) {
 	t.plan(2);
 
 	childProcess.execFile('./cli.js', ['--cwd=fixture'], {
 		cwd: __dirname
 	}, function (err, stdout) {
-		t.error(err);
+		t.ifError(err);
 		t.is(stdout.trim(), __dirname);
+		t.end();
 	});
 });
